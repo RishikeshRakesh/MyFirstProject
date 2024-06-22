ac8c7aa7efa80fd9744583d8510984f9c75e59a9
Rishikesh N <rishikeshsmart45@gmail.com>
2024-06-23 00:07:45 +0530

    calculator

---
+++ calci.js
@@ -0,0 +1,32 @@
+function add()
+{
+           let b= document.getElementById("value1").value;
+            let c=document.getElementById("value2").value;
+            let d=parseInt(b)+parseInt(c);
+            document.getElementById("value3").value=d;
+            
+    }
+    function sub()
+{
+           let b= document.getElementById("value1").value;
+            let c=document.getElementById("value2").value;
+            let d=parseInt(b)-parseInt(c);
+            document.getElementById("value3").value=d;
+            
+    }
+    function mul()
+{
+           let b= document.getElementById("value1").value;
+            let c=document.getElementById("value2").value;
+            let d=parseInt(b)*parseInt(c);
+            document.getElementById("value3").value=d;
+            
+    }
+    function div()
+{
+           let b= document.getElementById("value1").value;
+            let c=document.getElementById("value2").value;
+            let d=parseInt(b)/parseInt(c);
+            document.getElementById("value3").value=d;
+            
+    }
